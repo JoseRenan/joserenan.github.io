840255b6520c05eb75864788616cb2b201592098
José Renan <joserenansl99@gmail.com>
2018-11-17 15:45:12 -0300

    Initial commit

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,6 +6,46 @@ import './About.css';
 import Card from '../commons/card/Card';
 import Project from '../commons/project/Project';
 
+const projects = [
+    {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }, {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }, {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }, {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }, {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }, {
+        title: 'Teste',
+        description: 'Some description will be here!!!',
+        id: 'testing',
+        org: 'testing',
+        npm: true
+    }
+];
+
 const About = () => (
     <Section
         backgroundColor="#242424">
@@ -14,12 +54,7 @@ const About = () => (
             <div bp="8" className="projects">
                 <h1>Projects</h1>
                 <div bp="grid 6">
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
+                    {projects.map(project => <Project key={project.id} project={project}/>)}
                 </div>
             </div>
         </div>
